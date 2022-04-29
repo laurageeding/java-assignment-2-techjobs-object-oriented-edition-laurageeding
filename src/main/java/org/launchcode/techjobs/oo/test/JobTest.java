f09c228d114c4e2a01da48a4e893dc28405873ea
Laura Geeding <laurageeding@Lauras-MacBook-Pro.local>
2022-04-28 21:30:33 -0500

    initial commit

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,41 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
+
 public class JobTest {
+
+//    @Before
+//    public void init() {
+//        Job job1 = new Job();
+//        Job job2 = new Job();
+//    }
+
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertTrue(job2.getId() > job1.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job newJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(newJob instanceof Job);
+    }
+
+    @Test
+    public void testJobsForEquality() {
+    }
 
 }
